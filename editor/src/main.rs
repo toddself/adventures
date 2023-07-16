aab25a3bbf6421b99067c8cbb385b741af0ef565
Todd Kennedy <193412+toddself@users.noreply.github.com>
2023-07-16 13:00:32 -0700

    start working on editor

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,3 +1,29 @@
+use bevy::{prelude::*, window::WindowResolution};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        resolution: WindowResolution::new(
+                                        1700.0,
+                                        1110.0
+                        ),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_systems(Startup, (setup_camera, draw_ui))
+        .run();
+}
+
+fn setup_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+}
+
+fn draw_ui(mut commands: Commands) {
+
 }
